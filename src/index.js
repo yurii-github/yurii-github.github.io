491c929c8ff751a85d5904e7e14b7c63b1fe3b8f
Yurii K <yurii-github@users.noreply.github.com>
2017-07-02 17:48:33 +0300

    remove SW

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
 import './index.css';
 
-
 ReactDOM.render(<App/>, document.getElementById('root'));
-
-
-//import registerServiceWorker from './registerServiceWorker';
-//registerServiceWorker();
