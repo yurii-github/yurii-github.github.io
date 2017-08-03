28d8d265f75a307e5fe3bfe6f055688958c8d97b
Yurii K <yurii-github@users.noreply.github.com>
2017-08-03 20:49:33 +0300

    txt

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -132,15 +132,15 @@ export default class Homepage extends BasePage {
                     <p>All info here is my subject view on the field. Any comments are welcome. If you agree or not, maybe you have some intel that can change my view on things.</p>
 
                     <h2>PHP Frameworks</h2>
-                    <p>I do track some of PHP frameworks and CMS. I provide them with my comments in comparition table</p>
+                    <p>I do track some of PHP frameworks and CMS. I provide them with my comments in comparision table</p>
 
                     <h2>Patterns</h2>
-                    <p>Many of patterns are just regular OOP principles which people ignore to know. Instead, they create ton of useless retarded hipster names.</p>
+                    <p>Many of patterns are just regular OOP principles which people ignore to know. Instead, they create ton of useless hipster names.</p>
                     <p>To simplify things I provide aliases for patterns like 'Wrapper', because most of those patterns are simply basic or a bit complex wrappers over other objects or classes.</p>
                     <p>I've added to patterns so-called by hipsters 'antipatterns' because they still are patterns and are valid in many cases.</p>
 
                     <h2>Principles</h2>
-                    <p>Contains programming principles, at least, knows as. For most part it is just another hipster nonsense.</p>
+                    <p>Contains programming principles, at least, known as. For most part it is just another nonsense.</p>
 
 
                 </article>
