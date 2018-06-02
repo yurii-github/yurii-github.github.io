e884f22b5b186763a857a153f64a9436b4631f36
Yurii K <yurii-github@users.noreply.github.com>
2018-06-02 15:37:39 +0300

    created build 2018-06-02 15:37:39

--- build-pages.php
+++ build-pages.php
@@ -9,13 +9,10 @@ $pages = [
   'principles' => 'principles.php',
 ];
 
-
-
 foreach ($pages as $page => $link) {
     ob_start();
     require __DIR__."/web/{$page}.php";
     $html = ob_get_clean();
-    $htmlPage = "{$page}.html";
-    $html = str_replace($link, $htmlPage, $html);
-    file_put_contents("$buildDir/$htmlPage", $html);
+    $html = preg_replace('/href="(.*).php"/', 'href="${1}.html"', $html);
+    file_put_contents("$buildDir/{$page}.html", $html);
 }
