724f4d5988a04e13dc143344a0131652ffb3075a
Yurii K <yurii-github@users.noreply.github.com>
2019-10-13 04:33:38 +0300

    refactor

--- web/data/skills.php
+++ web/data/skills.php
@@ -1,58 +1,64 @@
 <?php
-
-return
-    <<<JSON
-[
-        {title: '<b>Storages</b>'},
-        {title: 'MySQL', rating: 8},
-        {title: 'mongoDB', rating: 1},
-        {title: 'Sqlite', rating: 4},
-        {title: 'Oracle ', rating: 0},
-        {title: 'postreSQL', rating: 1},
-        {title: 'MSSQL', rating: 1},
-        {title: 'Redis', rating: 1},
-        {title: '<b>Languages &amp; Markups</b>'},
-        {title: 'ASP .NET', rating: 2},
-        {title: 'Bash', rating: 5},
-        {title: 'C/C++/C#', rating: 2},
-        {title: 'CSS', rating: 7},
-        {title: 'Javascript', rating: 8},
-        {title: 'PHP', rating: 10},
-        {title: 'Python', rating: 1},
-        {title: 'SOAP/XML', rating: 5},
-        {title: 'React', rating: 2},
-        {title: 'RegExp', rating: 7},
-        {title: 'RESTful', rating: 7},
-        {title: 'Ruby Rails', rating: 0},
-        {title: 'Go', rating: 0},
-        
-        {title: '<b>PHP Frameworks</b>'},
-        {title: 'Yii 2', rating: 8},
-        {title: 'Symfony 2', rating: 2},
-        {title: 'Symfony 3', rating: 4},
-        {title: 'Symfony 4', rating: 1},
-        {title: 'Laravel 5', rating: 1},
-        {title: 'Joomla 3', rating: 1},
-        {title: 'PHPUnit', rating: 8},
-        {title: 'Codeception', rating: 6},
-        
-        {title: '<b>Collaboration</b>'},
-        {title: 'Mantis', rating: 1},
-        {title: 'JIRA', rating: 10},
-        {title: 'Redmine', rating: 2},
-        {title: 'Bugzilla', rating: 1},
-        {title: 'Git', rating: 10},
-        {title: 'SVN', rating: 5},
-        {title: 'Github', rating: 8},
-        {title: 'Gitlab', rating: 7},
-        {title: 'Bitbucket', rating: 7}, 
-        {title: 'TravisCI', rating: 6}, 
-        {title: '<b>Technologies</b>'},
-        {title: 'RabbitMQ', rating: 3},
-        {title: 'Bootstrap', rating: 7},
-        {title: 'Docker', rating: 6},        
-        {title: 'Jenkins', rating: 2},
-        {title: 'Gearman', rating: 0},
+$skills = [
+    'Storages' => [
+        ['MySQL', 8],
+        ['MSSQL', 0],
+        ['mongoDB', 3],
+        ['Sqlite', 8],
+        ['Oracle', 0],
+        ['PostgreSQL', 3],
+        ['Redis', 2],
+    ],
+    'Languages &amp; Markups' => [
+        ['ASP .NET', 2],
+        ['C/C++/C#', 4],
+        ['CSS', 5],
+        ['Javascript', 8],
+        ['PHP', 10],
+        ['SOAP/XML', 5],
+        ['RegExp', 5],
+        ['RESTful', 8],
+        ['Ruby Rails', 0],
+        ['Go', 0],
+    ],
+    'PHP Frameworks' => [
+        ['Yii 2', 8],
+        ['Symfony 2', 2],
+        ['Symfony 3', 3],
+        ['Symfony 4', 1],
+        ['Laravel 5', 10],
+        ['Joomla 3', 1],
+        ['PHPUnit', 5],
+        ['Codeception', 5],
+    ],
+    'Collaboration' => [
+        ['Mantis', 1],
+        ['JIRA', 10],
+        ['Redmine', 1],
+        ['Bugzilla', 1],
+        ['Git', 8],
+        ['SVN', 8],
+        ['Github', 8],
+        ['Gitlab', 8],
+        ['Bitbucket', 8],
+        ['TravisCI', 6],
+    ],
+    'Technologies' => [
+        ['RabbitMQ', 3],
+        ['Bootstrap', 4],
+        ['Docker', 5],
+        ['Jenkins', 1],
+        ['Gearman', 0],
     ]
-JSON
-    ;
+];
+
+$flatSkills = [];
+
+foreach ($skills as $group => $items) {
+    $flatSkills[] = ['title' => '<b>' . $group . '</b>'];
+    foreach ($items as &$item) {
+        $flatSkills[] = ['title' => $item[0], 'rating' => $item[1]];
+    }
+}
+
+return json_encode($flatSkills, JSON_UNESCAPED_UNICODE);
