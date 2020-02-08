b7e8ca66720a92f466bf444904808e5122386aad
Yurii K <yurii-github@users.noreply.github.com>
2020-02-08 13:09:06 +0200

    update

---
+++ web/data/principles.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    // abbr | full | desc
+    ['SRP', 'single responsibility principle'],
+    ['DIP', 'dependency injection principle'],
+    ['TDD', 'test driven development'],
+    ['BDD', 'behavior driven development'],
+    ['DRY', "don't repeat yourself"],
+    ['KISS', 'keep it simple, stupid (I would call it \'Keep it simply stupid\')'],
+    ['DIE', 'duplication is evil'],
+    ['XP', 'extreme programing'],
+    ['Agile', 'stolen RUP with faster cycles'],
+    ['Scrum', 'stolen RUP with faster cycles and process names created specifically for kids'],
+    ['SOLID', <<<HTML
+another nonsense from people without OOP background
+<br><b>S</b>ingle responsibility principle - A class should only have a single responsibility
+<br><b>O</b>pen–closed principle - Software entities ... should be open for extension, but closed for modification
+<br><b>L</b>iskov substitution principle - Objects in a program should be replaceable with instances of their subtypes 
+<br><b>I</b>nterface segregation principle - Many client-specific interfaces are better than one general-purpose interface
+<br><b>D</b>ependency inversion principle - One should "depend upon abstractions, not concretions
+HTML
+],
+    ['BoyScout Rule', 'leave code more clean - <i>one principle I think is good</i>'],
+    ['RUP', 'Rational Unified Process - <i>true iterative development process created by professionals, not someone\'s project on milking money on useless certifications</i>'],
+    ['Waterfall', 'people should return to <a href="https://www.youtube.com/watch?v=DMZzGynP2OE" target="_blank">waterfall</a>, relax and enjoy..']
+];
--- web/principles.php
+++ web/principles.php
@@ -1,27 +1,16 @@
 <?php
 $title = 'Principles';
 ob_start();
+$principles = require __DIR__ . '/data/principles.php';
 ?>
-<div class="container centered">
-    <h1><?php echo $title; ?></h1>
+  <div class="container centered">
+    <h1><?php echo $title; ?> (mostly hipster names)</h1>
     <article class="centered">
-        <p>List of principles (hipster names):</p>
-        <p><b>SRP</b> (single responsibility principle)</p>
-        <p><b>DIP</b> (dependency injection principle)</p>
-        <p><b>TDD</b> (test driven development)</p>
-        <p><b>BDD</b> (behavior driven development)</p>
-        <p><b>DRY</b> (Don't repeat yourself)</p>
-        <p><b>KISS</b> (Keep it simple, stupid), I would call it 'Keep it simply stupid'</p>
-        <p><b>DIE</b> (Duplication is evil)</p>
-        <p><b>XP</b> (extreme programing)</p>
-        <p><b>Agile</b> (stolen RUP with faster cycles)</p>
-        <p><b>Scrum</b> (stolen RUP with faster cycles and process names created specifically for kids)</p>
-        <p><b>SOLID</b> (another nonsense from people without OOP background)</p>
-        <p><b>* Boy Scout Rule</b> (leave code more clean) - <i>one principle I think is good</i></p>
-        <p><b>* RUP</b> (Rational Unified Process) - <i>true iterative development process created by professionals, not someone's project on milking money on useless certifications</i>
-        <p><b>Waterfall</b> (people should return to <a href="https://www.youtube.com/watch?v=DMZzGynP2OE" target="_blank">waterfall</a>, relax and enjoy..)</p>
+        <?php foreach ($principles as $principle): ?>
+          <p><b><?= $principle[0]; ?></b> - <?= $principle[1]; ?></p>
+        <?php endforeach; ?>
     </article>
-</div>
+  </div>
 <?php
 $content = ob_get_clean();
 require '_layout.php';
